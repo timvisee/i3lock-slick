b9ace32cdc89ac236578f8f237043c6f10ca9580
timvisee <timvisee@gmail.com>
2017-10-01 16:35:18 +0200

    Basics of invoking i3lock

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::process::Command;
+
 fn main() {
     println!("Hello, world!");
+
+    // Show the lock screen
+    lock();
+}
+
+fn lock() {
+    // Invoke i3lock
+    Command::new("i3lock")
+        .spawn()
+        .expect("Failed to invoke i3lock");
 }
